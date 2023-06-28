245cb352bfc1503e78e48dd8db1df8563c826c63
hungtp1996nd <hungtp1996nd@gmail.com>
2023-06-29 01:37:58 +0700

    add font and finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        notosans: 'NotoSansJP-Regular',
+      },
       colors: {
         header: "#414141",
         circle: "#FC7400",
